69010afeb4c4485f5d5cbdd0ee2998a7fa867085
leopsousa_ <leandro.sousa@beneagis.com>
2020-12-25 05:08:32 -0300

    step one

---
+++ src/Authentication/Onboarding/Onboarding.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View } from "react-native";
+
+// import { Container } from './styles';
+
+const Onboarding: React.FC = () => {
+  return <View style={{ flex: 1, backgroundColor: "cyan" }} />;
+};
+
+export default Onboarding;
